34d4dbec88e9fdbb19b81286e64dcfd11983c3e4
Sephixum <emotive.5353@gmail.com>
2023-05-19 01:10:22 +0330

    no of mic_stat

--- mic_stat.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python
-from subprocess import run
-from mic import status
-
-if status() == 'no':
-    run('polybar-msg action micstat hook 0', shell=True)
-elif status() == 'yes':
-    run('polybar-msg action micstat hook 1', shell=True)
